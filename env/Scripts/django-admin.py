dd61bb6c7e832537c322b32f075b45420502d8cd
root <root@tk2-404-43500.vs.sakura.ne.jp>
2019-11-07 14:14:27 +0900

    VPSからのコミット

--- env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\webapp\sales-gather\env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
--- manage.py
+++ manage.py
@@ -1,4 +1,5 @@
-#!/usr/bin/env python
+#!/bin/python3.6
+
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
