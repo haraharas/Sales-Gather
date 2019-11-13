c01f051c1e421a081718544a0639fa97d0d49e13
haraharas <s.uehara@offices.jp>
2019-11-13 10:15:09 +0900

    views.py

--- app/views.py
+++ app/views.py
@@ -36,9 +36,11 @@ class SaleFilterView(LoginRequiredMixin, FilterView):
 
 class SaleDetailView(LoginRequiredMixin, DetailView):
     model = Sale
-
+    success_url = reverse_lazy('index')
 
 # 登録画面
+
+
 class SaleCreateView(LoginRequiredMixin, CreateView):
     model = Sale
     form_class = SaleForm
@@ -60,27 +62,6 @@ class SaleDeleteView(LoginRequiredMixin, DeleteView):
 
 
 # 月集計
-class SaleMonthView(LoginRequiredMixin, FilterView):
+class SaleMonthView(LoginRequiredMixin, ListView):
     model = Sale
     template_name = "sale_month.html"
-    filterset_class = SaleFilter
-    # デフォルトの並び順を新しい順とする
-    queryset = Sale.objects.all().order_by('-created_at')
-
-    # クエリ未指定の時に全件検索を行うために以下のオプションを指定（django-filter2.0以降）
-    strict = False
-
-    # 1ページあたりの表示件数
-    paginate_by = 10
-
-    # 検索条件をセッションに保存する or 呼び出す
-    def get(self, request, **kwargs):
-        if request.GET:
-            request.session['query'] = request.GET
-        else:
-            request.GET = request.GET.copy()
-            if 'query' in request.session.keys():
-                for key in request.session['query'].keys():
-                    request.GET[key] = request.session['query'][key]
-
-        return super().get(request, **kwargs)
