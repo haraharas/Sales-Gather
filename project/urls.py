80bbbaef9861e222d5e95b26a4fd50eeb6ea3169
haraharas <s.uehara@offices.jp>
2019-12-03 17:45:27 +0900

    https化

--- project/urls.py
+++ project/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
@@ -21,3 +23,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls')),
 ]
+
+
+urlpatterns += static(settings.CERT_URL, document_root=settings.CERT_ROOT)
